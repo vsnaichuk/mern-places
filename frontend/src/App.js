96c43ca135feb03c743fe8a511c1c039324e28f0
v-sn <v.snaichuk@gmail.com>
2020-07-12 22:57:37 +0300

    Add Auth context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -10,41 +10,54 @@ import UpdatePlace from './places/containers/UpdatePlace';
 import UserPlaces from './places/containers/UserPlaces';
 import { routes } from './routes';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/authContext';
 import Auth from './user/containers/Auth';
 
 import Users from './user/containers/Users';
 
 const App = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <MainNavigation />
 
-      <main>
-        <Switch>
-          <Route path={routes.HOME} exact>
-            <Users />
-          </Route>
+        <main>
+          <Switch>
+            <Route path={routes.HOME} exact>
+              <Users />
+            </Route>
 
-          <Route path={routes.USER_PLACES} exact>
-            <UserPlaces />
-          </Route>
+            <Route path={routes.USER_PLACES} exact>
+              <UserPlaces />
+            </Route>
 
-          <Route path={routes.NEW_PLACE}>
-            <NewPlace />
-          </Route>
+            <Route path={routes.NEW_PLACE}>
+              <NewPlace />
+            </Route>
 
-          <Route path={routes.EDIT_PLACE}>
-            <UpdatePlace />
-          </Route>
+            <Route path={routes.EDIT_PLACE}>
+              <UpdatePlace />
+            </Route>
 
-          <Route path={routes.AUTH}>
-            <Auth />
-          </Route>
+            <Route path={routes.AUTH}>
+              <Auth />
+            </Route>
 
-          <Redirect to={routes.HOME} />
-        </Switch>
-      </main>
-    </Router>
+            <Redirect to={routes.HOME} />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
