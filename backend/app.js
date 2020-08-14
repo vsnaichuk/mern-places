676a7cf1ed9bd001964b47c7f4b6ab300eb4a558
v-sn <v.snaichuk@gmail.com>
2020-08-15 01:50:50 +0300

    Change cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,19 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: true, credentials: true }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE',
+  );
+
+  next();
+});
 
 app.use(bodyParser.json());
 
