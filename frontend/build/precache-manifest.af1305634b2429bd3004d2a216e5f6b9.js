de1f6c880992631d64f609eb20a680bbc512b902
v-sn <v.snaichuk@gmail.com>
2020-08-15 11:52:42 +0300

    Change db

---
+++ frontend/build/precache-manifest.af1305634b2429bd3004d2a216e5f6b9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e93a092bc3bc5f669c940a8d46b8087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d8e2cb367b3026536f",
+    "url": "/static/css/0.dd752b5a.chunk.css"
+  },
+  {
+    "revision": "0b83432aa055bf873fbb",
+    "url": "/static/css/4.ff8dd44c.chunk.css"
+  },
+  {
+    "revision": "acc36170d58e22b77ced",
+    "url": "/static/css/5.dd095864.chunk.css"
+  },
+  {
+    "revision": "8d3593c550ee232f6161",
+    "url": "/static/css/6.1258d7a5.chunk.css"
+  },
+  {
+    "revision": "c5050628d590a44581b6",
+    "url": "/static/css/7.a7831852.chunk.css"
+  },
+  {
+    "revision": "98521cf26ee6f35c45fb",
+    "url": "/static/css/8.487cadd3.chunk.css"
+  },
+  {
+    "revision": "7eea69c10bbfde8e9c63",
+    "url": "/static/css/main.58c3c8bd.chunk.css"
+  },
+  {
+    "revision": "79d8e2cb367b3026536f",
+    "url": "/static/js/0.7400ffe3.chunk.js"
+  },
+  {
+    "revision": "32f57c464c4fc482f3c4",
+    "url": "/static/js/3.5bc40755.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.5bc40755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b83432aa055bf873fbb",
+    "url": "/static/js/4.75de5877.chunk.js"
+  },
+  {
+    "revision": "acc36170d58e22b77ced",
+    "url": "/static/js/5.9888d96b.chunk.js"
+  },
+  {
+    "revision": "8d3593c550ee232f6161",
+    "url": "/static/js/6.8fd8ca8a.chunk.js"
+  },
+  {
+    "revision": "c5050628d590a44581b6",
+    "url": "/static/js/7.ef2d3c35.chunk.js"
+  },
+  {
+    "revision": "98521cf26ee6f35c45fb",
+    "url": "/static/js/8.9a13c636.chunk.js"
+  },
+  {
+    "revision": "7eea69c10bbfde8e9c63",
+    "url": "/static/js/main.9b205e0c.chunk.js"
+  },
+  {
+    "revision": "c7e76a87aba9bedc23d4",
+    "url": "/static/js/runtime-main.f6bbc40c.js"
+  }
+]);
