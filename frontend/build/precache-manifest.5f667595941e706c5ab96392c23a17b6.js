e74f71514bb116988c91ac8406bf8b5eb148bea2
v-sn <v.snaichuk@gmail.com>
2020-08-15 11:03:58 +0300

    Setup file upload on backend

---
+++ frontend/build/precache-manifest.5f667595941e706c5ab96392c23a17b6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf45a7c2c410fe09031c2f639772994f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d8e2cb367b3026536f",
+    "url": "/static/css/0.dd752b5a.chunk.css"
+  },
+  {
+    "revision": "e09bd022a4a38863a6eb",
+    "url": "/static/css/4.ff8dd44c.chunk.css"
+  },
+  {
+    "revision": "9a3c4466e6bcc500b5c6",
+    "url": "/static/css/5.dd095864.chunk.css"
+  },
+  {
+    "revision": "8d3593c550ee232f6161",
+    "url": "/static/css/6.1258d7a5.chunk.css"
+  },
+  {
+    "revision": "c5050628d590a44581b6",
+    "url": "/static/css/7.a7831852.chunk.css"
+  },
+  {
+    "revision": "98521cf26ee6f35c45fb",
+    "url": "/static/css/8.487cadd3.chunk.css"
+  },
+  {
+    "revision": "7eea69c10bbfde8e9c63",
+    "url": "/static/css/main.58c3c8bd.chunk.css"
+  },
+  {
+    "revision": "79d8e2cb367b3026536f",
+    "url": "/static/js/0.7400ffe3.chunk.js"
+  },
+  {
+    "revision": "32f57c464c4fc482f3c4",
+    "url": "/static/js/3.5bc40755.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.5bc40755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09bd022a4a38863a6eb",
+    "url": "/static/js/4.500308e7.chunk.js"
+  },
+  {
+    "revision": "9a3c4466e6bcc500b5c6",
+    "url": "/static/js/5.3917b18d.chunk.js"
+  },
+  {
+    "revision": "8d3593c550ee232f6161",
+    "url": "/static/js/6.8fd8ca8a.chunk.js"
+  },
+  {
+    "revision": "c5050628d590a44581b6",
+    "url": "/static/js/7.ef2d3c35.chunk.js"
+  },
+  {
+    "revision": "98521cf26ee6f35c45fb",
+    "url": "/static/js/8.9a13c636.chunk.js"
+  },
+  {
+    "revision": "7eea69c10bbfde8e9c63",
+    "url": "/static/js/main.9b205e0c.chunk.js"
+  },
+  {
+    "revision": "0905736dbc7d6058d3e8",
+    "url": "/static/js/runtime-main.fc562a7d.js"
+  }
+]);
